981c9fb213fa7004244d9e2321440ddc185c7ce0
Pranav220308 <21377@students.lynfield.school.nz>
2024-06-17 22:52:24 +1200

    Added a quit button, main function and defined the window size

--- Concept 1.py
+++ Concept 1.py
@@ -4,5 +4,20 @@
 
 #Create the GUI
 from tkinter import *
-import tkinter
-window = tkinter.Tk()
+main_window = Tk()
+main_window.geometry("600x120")
+
+#Quit the program
+def quit():
+    main_window.destroy()
+
+#Create the buttons and labels
+    Button(main_window,command=quit, text="Quit",bg="#ba2106",fg = "#FFFFFF",width=12) .grid(column=12, row=4, sticky=E)
+
+
+
+
+#Start the program running
+def main():
+    main_window.mainloop()
+main()
