a50c498432c17f8e2d5bae8357dffae0ed51fd67
Pranav220308 <21377@students.lynfield.school.nz>
2024-06-17 21:35:17 +1200

    Create the GUI

--- Concept 1.py
+++ Concept 1.py
@@ -1,2 +1,8 @@
-#import tkinter so a GUI can be made
+#Date: 17/06/2024
+#AUthor: Pranav Kumar
+#Purpose: To create a GUI which allows the user to enter details, append their details and allows them to quit the program. 
+
+#Create the GUI
 from tkinter import *
+import tkinter
+window = tkinter.Tk()
